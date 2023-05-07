d93b21ad27032524fe868678390a66ae675c7917
DK@Macbook <iamjdg11@gmail.com>
2023-05-07 15:48:14 +0900

    errata in messages are corrected

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ async function main() {
 
     selectInputType(0);
     const message =
-      "집사님! 기존에 입력하신 반려동물의 정보를 확인해습니다. 아래 기존 정보를 그대로 사용할까요?";
+      "집사님! 기존에 입력하신 반려동물의 정보를 확인했습니다. 아래 정보를 그대로 사용할까요?";
     await createMessage(message, "assistant");
     await createMessage(`"${preUserMessageCached}"`, "assistant");
   } else {
@@ -154,7 +154,7 @@ function inputDisabled(isDisable) {
     //Enable
     chatInput.disabled = false;
     chatInput.placeholder =
-      "여기에 질문을 입력하신 후에 오른쪽 [보내기] 버튼을 클릭해 주세요~ ^^";
+      "여기에 질문을 입력하신 후, 오른쪽 [보내기] 버튼을 클릭해 주세요~ ^^";
     chatInput.style.backgroundColor = "white";
     inputButton.disabled = false;
     inputButton.style.color = "rgb(200, 200, 200)";
@@ -240,7 +240,7 @@ async function showAssistantFetching() {
 }
 
 const errorMessage = [
-  "어이쿠.. 😅\n 답변을 생성하다가 이유를 모를 문제가 발생했어요. 죄송하지만 한 번더 시도해 보시겠어요? 🥺.",
+  "어이쿠.. 😅\n 답변을 생성하다가 문제가 발생했어요. 죄송하지만 한 번더 시도해 보시겠어요? 🥺.",
   "헉.. 에러가 또 발생했네요.. 😢😢😢.",
 ];
 // let isFirstError = true;
